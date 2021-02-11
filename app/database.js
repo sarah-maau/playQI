2b20272bf5ac4863594231a8d1da80dba0d23285
sarah-maau <maurysarah@hotmail.fr>
2021-02-11 16:54:01 +0100

    mise en ligne heroku

--- app/database.js
+++ app/database.js
@@ -1,7 +1,8 @@
 require('dotenv').config();
 
 const { Sequelize } = require('sequelize');
-const sequelize = new Sequelize(process.env.PG_URL, {
+const sequelize = new Sequelize(
+    {connectionString: process.env.DATABASE_URL}, {
     define: {
         timestamps: false
     }
